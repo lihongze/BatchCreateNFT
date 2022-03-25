1d86cdb27591bf8daac448e85e9964fd5d75a1b6
lihongze <1713725007@qq.com>
2022-03-25 15:24:36 +0800

    add some help method

--- src/main/java/com/batchcreate/nft/utils/ImageUtil.java
+++ src/main/java/com/batchcreate/nft/utils/ImageUtil.java
@@ -18,8 +18,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class ImageUtil {
-    public static String SAVE_PATH = "D:\\NFT-3-9\\pic\\";
-    public static String META_DATA_PATH = "D:\\NFT-3-9\\metadata\\";
+    public static String SAVE_PATH = "D:\\NFT-FINISH\\version-1.0\\多数基础款-图\\pic\\";
+    public static String META_DATA_PATH = "D:\\NFT-FINISH\\version-1.0\\多数基础款-图\\metadata\\";
 
     public static AsyncTaskExecutor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
@@ -84,11 +84,12 @@ public class ImageUtil {
         // init nft token number
         AtomicInteger integer = new AtomicInteger(0);
         // i means nft quantity
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 150000; i++) {
+            log.info("循环第{}次", i);
             // concurrency create ,you can specify the layer order according to your needs
             executor.execute(() -> {
                         try {
-                            BufferedImage buffImage = ImageAddWord(1500, 1500);
+                            BufferedImage buffImage = ImageAddWord(1000, 1000);
                             List<Map<String, String>> metaData = new ArrayList<>();
 
                             Graphics2D g = buffImage.createGraphics();
@@ -101,7 +102,7 @@ public class ImageUtil {
                             int gg = random.nextInt(heads.length);
                             int ff = random.nextInt(hands.length);
 
-                            String s = a + b + c + d + e + f + gg + String.valueOf(ff);
+                            String s = a + b + c + d  + f + gg + String.valueOf(ff);// + e
                             // anti-duplication
                             if (list.contains(s)) {
                                 return;
@@ -109,21 +110,21 @@ public class ImageUtil {
                             list.add(s);
 
                             File back = backgrounds[a];
-                            g.drawImage(ImageIO.read(back).getScaledInstance(1500, 1500, Image.SCALE_SMOOTH), 0, 0, null);
+                            g.drawImage(ImageIO.read(back).getScaledInstance(1000, 1000, Image.SCALE_SMOOTH), 0, 0, null);
                             Map<String, String> metaMap = new HashMap<>(2);
                             metaMap.put("trait_type", "background");
                             metaMap.put("value", back.getName().substring(0, back.getName().lastIndexOf(".")));
                             metaData.add(metaMap);
 
                             File people = peoples[b];
-                            g.drawImage(ImageIO.read(people).getScaledInstance(1500, 1500, Image.SCALE_SMOOTH), 0, 0, null);
+                            g.drawImage(ImageIO.read(people).getScaledInstance(1000, 1000, Image.SCALE_SMOOTH), 0, 0, null);
                             Map<String, String> metaMap1 = new HashMap<>(2);
                             metaMap1.put("trait_type", "body");
                             metaMap1.put("value", people.getName().substring(0, people.getName().lastIndexOf(".")));
                             metaData.add(metaMap1);
 
                             File mouth = mouths[d];
-                            g.drawImage(ImageIO.read(mouth).getScaledInstance(1500, 1500, Image.SCALE_SMOOTH), 0, 0, null);
+                            g.drawImage(ImageIO.read(mouth).getScaledInstance(1000, 1000, Image.SCALE_SMOOTH), 0, 0, null);
                             Map<String, String> metaMap3 = new HashMap<>(2);
                             metaMap3.put("trait_type", "mouth");
                             metaMap3.put("value", mouth.getName().substring(0, mouth.getName().lastIndexOf(".")));
@@ -132,7 +133,7 @@ public class ImageUtil {
                             //                            specify the probability of occurrence of a layer
                             if (random.nextInt(100) >= 10) {
                                 File bodyTip2 = tips[e];
-                                g.drawImage(ImageIO.read(bodyTip2).getScaledInstance(1500, 1500, Image.SCALE_SMOOTH), 0, 0, null);
+                                g.drawImage(ImageIO.read(bodyTip2).getScaledInstance(1000, 1000, Image.SCALE_SMOOTH), 0, 0, null);
                                 Map<String, String> metaMap4 = new HashMap<>(2);
                                 metaMap4.put("trait_type", "body-tag");
                                 metaMap4.put("value", bodyTip2.getName().substring(0, bodyTip2.getName().lastIndexOf(".")));
@@ -140,28 +141,28 @@ public class ImageUtil {
                             }
 
                             File glass = glasses[f];
-                            g.drawImage(ImageIO.read(glass).getScaledInstance(1500, 1500, Image.SCALE_SMOOTH), 0, 0, null);
+                            g.drawImage(ImageIO.read(glass).getScaledInstance(1000, 1000, Image.SCALE_SMOOTH), 0, 0, null);
                             Map<String, String> metaMap5 = new HashMap<>(2);
                             metaMap5.put("trait_type", "eye");
                             metaMap5.put("value", glass.getName().substring(0, glass.getName().lastIndexOf(".")));
                             metaData.add(metaMap5);
 
                             File cloth = cloths[c];
-                            g.drawImage(ImageIO.read(cloth).getScaledInstance(1500, 1500, Image.SCALE_SMOOTH), 0, 0, null);
+                            g.drawImage(ImageIO.read(cloth).getScaledInstance(1000, 1000, Image.SCALE_SMOOTH), 0, 0, null);
                             Map<String, String> metaMap2 = new HashMap<>(2);
                             metaMap2.put("trait_type", "cloth");
                             metaMap2.put("value", cloth.getName().substring(0, cloth.getName().lastIndexOf(".")));
                             metaData.add(metaMap2);
 
                             File head = heads[gg];
-                            g.drawImage(ImageIO.read(head).getScaledInstance(1500, 1500, Image.SCALE_SMOOTH), 0, 0, null);
+                            g.drawImage(ImageIO.read(head).getScaledInstance(1000, 1000, Image.SCALE_SMOOTH), 0, 0, null);
                             Map<String, String> metaMap6 = new HashMap<>(2);
                             metaMap6.put("trait_type", "head");
                             metaMap6.put("value", head.getName().substring(0, head.getName().lastIndexOf(".")));
                             metaData.add(metaMap6);
 
                             File hand = hands[ff];
-                            g.drawImage(ImageIO.read(hand).getScaledInstance(1500, 1500, Image.SCALE_SMOOTH), 0, 0, null);
+                            g.drawImage(ImageIO.read(hand).getScaledInstance(1000, 1000, Image.SCALE_SMOOTH), 0, 0, null);
                             Map<String, String> metaMap7 = new HashMap<>(2);
                             metaMap7.put("trait_type", "hand");
                             metaMap7.put("value", hand.getName().substring(0, hand.getName().lastIndexOf(".")));
@@ -171,12 +172,15 @@ public class ImageUtil {
                             int i1 = integer.addAndGet(1);
                             String filepath = SAVE_PATH + i1 + ".png";
                             String metaPath = META_DATA_PATH + i1 + ".token.json";
-                            String metaJson = String.format("{\"name\":\"Token %s\",\"description\":\"An art token.\",\"animation_url\":\"/assets/index.html?id=%s\",\"image\":\"/assets/thumbnail/%s.png\",\"attributes\":%s}",
-                                    i1, i1, i1, JsonUtils.object2Json(metaData));
+                            String metaJson = String.format("{\"name\":\"Token %s\",\"description\":\"An art token.\",\"image\":\"/%s.png\",\"attributes\":%s}",
+                                    i1, i1, JsonUtils.object2Json(metaData));
                             // generate images and metadata
                             imageWriter.write(buffImage, new File(filepath), metaPath, metaJson);
                             buffImage.flush();
                             log.info("生成图片{}张", i1);
+                            if(i1 >= 8500) {
+                                System.exit(0);
+                            }
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -184,6 +188,57 @@ public class ImageUtil {
             );
         }
     }
+
+
+    public static void resetSize() {
+        File[] images = new File("D:\\NFT-3-7-定稿\\pic-1\\").listFiles();
+        Arrays.stream(Objects.requireNonNull(images)).forEach(image -> {
+            try {
+                BufferedImage buffImage = ImageIO.read(image);
+                ImageIO.write(resizeImageWithHint(buffImage),"png",new File(image.getAbsolutePath()));
+                log.info("resetSize path :{}",image.getAbsolutePath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+
+    /**
+     * 重置图片大小
+     */
+    private static BufferedImage resizeImageWithHint(BufferedImage originalImage){
+        BufferedImage resizedImage = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(originalImage, 0, 0, 1000, 1000, null);
+        g.dispose();
+        g.setComposite(AlphaComposite.Src);
+
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g.setRenderingHint(RenderingHints.KEY_RENDERING,
+                RenderingHints.VALUE_RENDER_QUALITY);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        return resizedImage;
+    }
+
+    /**
+     * 根据要删除得图片集 删除对应的metadata数据
+     */
+    public static void deleteMetadataByImage() {
+        String deleteImageUrl = "D:\\NFT-FINISH\\version-1.0\\test\\delete\\";
+        String metadataUrl = "D:\\NFT-FINISH\\version-1.0\\test\\metaData\\";
+        File[] images = new File(deleteImageUrl).listFiles();
+        Arrays.stream(Objects.requireNonNull(images)).forEach(file -> {
+            String number = file.getName().substring(0, file.getName().lastIndexOf("."));
+            File delFile = new File(metadataUrl + number + ".token.json");
+            delFile.delete();
+            log.info("删除文件：{}",delFile.getAbsolutePath());
+        });
+    }
+
 
     /**
      * 设置源图片为背景透明，并设置透明度
@@ -232,8 +287,12 @@ public class ImageUtil {
 
     @SneakyThrows
     public static void main(String[] args) {
-        ImageUtil.batchDrawn();
+        // 生成图片
+//        ImageUtil.batchDrawn();
+        // 重置size
+//        ImageUtil.resetSize();
+        // 删除文件
+        ImageUtil.deleteMetadataByImage();
     }
-
 
 }
